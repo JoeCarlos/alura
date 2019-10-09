dd740b1bafb6c52a819dd71ddd1e5378a3afd84c
Joe Yang <jose.ozelzo10@gmail.com>
2019-10-08 23:37:40 -0300

    Inicio Aula 2 Design Patterns

---
+++ design-patterns/src/Item.java
@@ -0,0 +1,19 @@
+
+public class Item {
+	private String nome;
+	private double valor;
+
+	public Item (String nome, double valor) {
+		this.nome = nome;
+		this.valor = valor;
+		
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+}
