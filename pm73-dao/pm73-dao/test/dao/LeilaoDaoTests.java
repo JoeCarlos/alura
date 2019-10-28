7e8091c485ae1a317fff8318a16632cc82eb5507
jocas <jose.carlos@bndes.gov.br>
2019-10-28 12:54:22 -0300

    66% Curso de Teste de Integração

--- pm73-dao/pm73-dao/test/dao/LeilaoDaoTests.java
+++ pm73-dao/pm73-dao/test/dao/LeilaoDaoTests.java
@@ -2,9 +2,13 @@ package dao;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Calendar;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import br.com.caelum.pm73.dao.CriadorDeSessao;
 import br.com.caelum.pm73.dao.LeilaoDao;
@@ -17,27 +21,31 @@ public class LeilaoDaoTests {
 	private Session session;
 	private UsuarioDao usuarioDao;
 	private LeilaoDao leilaoDao;
-	
+	private Usuario usuario;
+	private Leilao leilao;
 	
 	@Before
 	public void setUp() {
 		session = new CriadorDeSessao().getSession();
 		usuarioDao = new UsuarioDao(session);
 		leilaoDao = new LeilaoDao(session);
-	}
+		session.beginTransaction();
+		usuario = new Usuario("Joe", "jose@gmail.com");
+		usuarioDao.salvar(usuario);
+		leilao = new Leilao("Xbox", 500.00, usuario, false);
+		leilaoDao.salvar(leilao);
+		}
 	
 	@After
 	public void encerra() {
+		session.getTransaction().rollback();
 		session.close();
 	}
 
+	@Test
 	public void deveContarLeiloesNaoEncerrados() {
-		Usuario usuario = new Usuario("Joe", "jose@gmail.com");
-		Leilao ativo = new Leilao("Carro", 21.000, usuario , false);
 		Leilao encerrado = new Leilao("Aparelho", 2.000, usuario, false);
-		
-		usuarioDao.salvar(usuario);
-		leilaoDao.salvar(ativo);
+		encerrado.encerra();
 		leilaoDao.salvar(encerrado);
 		
 		long total = leilaoDao.total();
@@ -45,6 +53,73 @@ public class LeilaoDaoTests {
 		assertEquals(1L, total);
 		
 	}
+	
+	@Test
+	public void totalLeiloes0Test() {
+		Leilao encerrado2 = new Leilao("Carro", 21.000, usuario , false);
+		leilaoDao.salvar(encerrado2);
+		leilao.encerra();
+		encerrado2.encerra();
+		
+		long total = leilaoDao.total();
+		
+		assertEquals(0L, total);
+	
+	}
+	
+	@Test
+	public void deveRetornarLeiloesNaoUtilizados() {
+		Leilao leilao2 = new Leilao("PS4", 1200.00, usuario, true);
+		leilaoDao.salvar(leilao2);
+		
+		assertEquals(1L, leilaoDao.novos().size());
+		assertEquals("Xbox", leilaoDao.novos().get(0).getNome());
+	}
 
+	@Test 
+	public void deveRetornarLeiloesAntigos(){
+		Leilao leilaoAntigo = new Leilao("Morte", 10000.0, usuario, false);
+		leilao.setDataAbertura(Calendar.getInstance());
+		Calendar dataAntiga = Calendar.getInstance();
+		dataAntiga.add(Calendar.DAY_OF_MONTH, -10);
+		leilaoAntigo.setDataAbertura(dataAntiga);
+		leilaoDao.salvar(leilaoAntigo);
+		
+		assertEquals(1L, leilaoDao.antigos().size());
+		assertEquals("Morte", leilaoDao.antigos().get(0).getNome());
+	}
+	
+	@Test 
+	public void retornarLeilaoCriadoHaExatamente7Dias() {
+		Calendar dataAbertura = Calendar.getInstance();
+		dataAbertura.add(Calendar.DAY_OF_MONTH, -7);
+		leilao.setDataAbertura(dataAbertura);
+		  List<Leilao> antigos = leilaoDao.antigos();
+		assertEquals(1, leilaoDao.antigos().size());
+	}
 
+	@Test
+	public void retornaLeiloesNaoEncerradosDentroDeUmPeriodo() {
+		
+		leilao.setDataAbertura(Calendar.getInstance());
+		Leilao leilao2 = new Leilao("Carro", 30.000, usuario, false);
+	
+		Calendar foraDoIntervalo = Calendar.getInstance();
+		foraDoIntervalo.add(Calendar.MONTH, -30);
+		leilao2.setDataAbertura(foraDoIntervalo);
+		
+		leilaoDao.salvar(leilao2);
+//		leilao2.encerra();
+		Calendar dataInicio = Calendar.getInstance();
+		dataInicio.add(Calendar.MONTH, -1);
+		List<Leilao> porPeriodo = leilaoDao.porPeriodo(dataInicio, Calendar.getInstance());
+		
+		assertEquals(1L, porPeriodo.size());
+		assertEquals("Xbox", porPeriodo.get(0).getNome());
+//		assertEquals("Carro", porPeriodo.get(1).getNome());
+
+	}
+	
+//	@Test
+//	public void 
 }
